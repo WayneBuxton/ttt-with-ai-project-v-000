e6bfb8d98ce4edcd2a4677c91f280fb61d50d618
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-02-09 18:07:15 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,7 @@ Bundler.require
 
 
 require_relative '../lib/board'
+require_relative '../lib/game'              
+require_relative '../lib/player'
+require_relative '../lib/players/computer'
+require_relative '../lib/players/human'
