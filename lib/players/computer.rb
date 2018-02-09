b06bd3794e94a747324388544aaf4f8796de2662
Wayne Buxton <waynebuxton2184@yahoo.com>
2018-02-09 16:46:55 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -20,8 +20,7 @@ module Players
         if board.turn_count == 4 && board.cells[0,3] == "X" && !board.taken?(5) return "5"
         if board.turn_count == 4 && board.cells[0,1] == "X" && !board.taken?(3) return "3"
         if board.turn_count == 4 && board.cells[0,1] == "X" && !board.taken?(5) return "5"
-        elsif board.turn_count == 4 && board.cells[0,1] == "X" && !board.taken?(7)
-          "7"
+        if board.turn_count == 4 && board.cells[0,1] == "X" && !board.taken?(7) return "7"
         elsif board.turn_count == 4 && board.cells[0,4] == "X" && !board.taken?(9)
           "9"
         elsif board.turn_count == 4 && board.cells[0,4] == "X" && !board.taken?(3)
